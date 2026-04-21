29cc855633d68fe2cf32f8efe5d9bc8c62a6b469
oussamamalih <osmmlh816@gmail.com>
2026-04-21 12:57:15 +0100

    add sm and fix sm prblms

--- app.js
+++ app.js
@@ -2,22 +2,26 @@ const products = [
   {
     name: "Huile Extra Vierge Classique",
     price: "200 MAD",
-    image: "images/pro1.png"
+    image: "images/pro1.png",
+    desc: "Notre huile d'olive extra vierge classique est un véritable trésor de la nature, pressée à froid pour préserver toutes ses qualités. Avec son goût riche et équilibré, elle est parfaite pour sublimer vos plats et apporter une touche méditerranéenne à votre cuisine."
   },
   {
     name: "Huile Bio Premium 500ml",
     price: "80 MAD",
-    image: "images/pro2.png"
+    image: "images/pro2.png",
+    desc: "Notre huile d'olive extra vierge classique est un véritable trésor de la nature, pressée à froid pour préserver toutes ses qualités. Avec son goût riche et équilibré, elle est parfaite pour sublimer vos plats et apporter une touche méditerranéenne à votre cuisine."
   },
   {
     name: "Huile Artisanale 1L",
     price: "80 MAD",
-    image: "images/pro3.png"
+    image: "images/pro3.png",
+    desc: "Notre huile d'olive extra vierge classique est un véritable trésor de la nature, pressée à froid pour préserver toutes ses qualités. Avec son goût riche et équilibré, elle est parfaite pour sublimer vos plats et apporter une touche méditerranéenne à votre cuisine."
   },
   {
     name: "Coffret Découverte",
     price: "80 MAD",
-    image: "images/pro4.png"
+    image: "images/pro4.png",
+    desc: "Notre huile d'olive extra vierge classique est un véritable trésor de la nature, pressée à froid pour préserver toutes ses qualités. Avec son goût riche et équilibré, elle est parfaite pour sublimer vos plats et apporter une touche méditerranéenne à votre cuisine."
   }
 ];
 
@@ -33,6 +37,7 @@ products.forEach(product => {
     <img src="${product.image}" alt="${product.name}">
     <h3>${product.name}</h3>
     <p class="prix">${product.price}</p>
+    <p class="desc">${product.desc}</p>
   <button class="add-btn" id="add-btn">Add to Cart</button>
   `;
 
